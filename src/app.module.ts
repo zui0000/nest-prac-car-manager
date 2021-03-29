afe2591cb04d59e15ed248fbbea153fc683dcc0b
zui <shikisokuzekuu.999@gmail.com>
2021-03-30 00:16:42 +0900

    mongodbをインストールするところまで

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CarModule } from './car/car.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/car_manager'),
+    CarModule
+  ],
   controllers: [],
   providers: [],
 })
